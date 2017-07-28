2e386d2aead9d04b1f7473cb744a830ded083309
Jason Zhuang <aajz@live.cn>
2017-07-28 11:46:22 +0800

    gogogo

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,21 @@
 package com.company;
-
+import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("cnmd!");
-        string abc="";
-
+        Random generator=new Random();
+        int value=generator.nextInt();
+        int tryeqal=generator.nextInt();
+        tryeqal=value;
+        System.out.println(tryeqal);
+        String searchKeywords="let's count me ! hahahahaahah";
+        searchKeywords += " wtf?";
+        System.out.println(searchKeywords);
+        int nameLength=searchKeywords.length();
+        System.out.println(nameLength);
+        String letsUpper=searchKeywords.toUpperCase();
+        System.out.println(letsUpper);
     }
 }
